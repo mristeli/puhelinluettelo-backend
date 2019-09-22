1ec8424e18a3af2b4adac53bad7b046cc50ef450
Matti Risteli <matti.risteli@iki.fi>
2019-09-22 22:15:04 +0300

    3.6: Basic REST functionaly done, testset for VC Code RESTclient in requests

---
+++ index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express() 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if(person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+})
+
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10**9)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+        error: `${body.name ? 'Number' : 'Name'}  is missing`
+    })
+  } else if (persons.find(n => n.name === body.name)) {
+    return response.status(400).json({
+        error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    ...body,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  response.send('<html><head></head><body>' 
+    + `<p>Phonebook has info for ${persons.length} people<p>` 
+    + `<p>${new Date()}</p>`
+    + '</body></html>').end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
