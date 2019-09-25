e833b6bb531fdeab3e4e2aabfc763bd159551cd8
Matti Risteli <matti.risteli@iki.fi>
2019-09-25 20:30:56 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # puhelinluettelo-backend
 Separate repository for the backend of puhelinluettelo https://github.com/mristeli/fullstack/tree/master/osa2/puhelinluettelo
+
+Running at https://limitless-falls-99237.herokuapp.com/
+	- try e. g. paths /info, /api/persons
---
+++ build/precache-manifest.ab3df742d5ebd9d65136de236ca4c4c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9cf57ad2bdf5ea104c4370746738f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fc558f3162ab8318e38",
+    "url": "/static/css/main.eb854605.chunk.css"
+  },
+  {
+    "revision": "a0210ab1b5b72d3108e6",
+    "url": "/static/js/2.09ccb4a8.chunk.js"
+  },
+  {
+    "revision": "1fc558f3162ab8318e38",
+    "url": "/static/js/main.d090b155.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
